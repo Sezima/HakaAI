5e9ab0ead47bf5f40ac5a84b226067b50fa617a8
sezim <osmsezimka@gmail.com>
2023-08-05 02:52:45 +0600

    added swagger

--- hakaAI/hakaAI/settings.py
+++ hakaAI/hakaAI/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -73,6 +75,7 @@ WSGI_APPLICATION = "hakaAI.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# we will use PSQL
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
